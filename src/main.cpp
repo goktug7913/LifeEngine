926a3558d54141d73c89ecb985c5f15c895148b2
Goktug Yuksel <goktug7913@gmail.com>
2024-08-31 01:02:31 +0300

    CLEANUP

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
-#include "Engine/engine.h"
+#include "raylib.h"
+
+struct properties
+{
+  int width = 1280;
+  int height = 720;
+  const char *title = "LifeEngine";
+  int fps = 120;
+};
 
 int main(int argc, const char **argv)
 {
-  Engine engine;
-  engine.run();
+  properties properties;
+  InitWindow(properties.width, properties.height, properties.title);
+  SetTargetFPS(properties.fps);
+
+  while (!WindowShouldClose())
+  {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    EndDrawing();
+  }
+
   return 0;
 }
